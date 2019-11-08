c873ff709079a45ba27330cc516c8eeffb3bc42e
VasileDochita <54108503+VasileDochita@users.noreply.github.com>
2019-11-08 15:45:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompletareTema2Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalLinii;
+            Console.WriteLine("Insert file adress");
+            var b = Console.ReadLine();
+            string[] name = File.ReadAllLines(b);
+            totalLinii = int.Parse(name[0]) + int.Parse(name[1]);
+            var a = new StreamWriter(b, true);
+            a.WriteLine("\n Sum: " + totalLinii);
+            a.Close();
+            Console.ReadLine();
+        }
+    }
+}
